801745325993a63b4ffcfcab7a7b8f1113a75b7d
clabnet <clabnet@users.noreply.github.com>
2023-09-07 12:21:12 +0200

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,24 +1,67 @@
 import './style.css';
-import * as _ from 'lodash';
-import bom from './data/sourceData.json';
 
-// const json0 = JSON.stringify(data, null, 2);
+import { h, ref, computed, watchEffect } from 'vue';
+import { MenuOption } from 'naive-ui';
+
+import * as _ from 'lodash';
+import bomTree from './data/bomtree.json';
+
+export interface Menu {
+  id: string;
+  label: string;
+  icon?: string;
+  type?: string;
+  name?: string;
+  description?: string;
+  level: string;
+  params?: { [key: string]: string };
+  children?: Menu[];
+}
+
+const mapping = (items: Menu[]): MenuOption[] =>
+  items.map((item) => ({
+    ...item,
+    key: item.id,
+    label: item.name,
+    // children: mapping(item),
+  }));
+
+// generazione
+const options = bomTree ? mapping(bomTree) : [];
+
+// menu data
+const json1 = JSON.stringify(options, null, 2);
+console.log(json1);
+
+// API original data
+const json0 = JSON.stringify(bomTree, null, 2);
+console.log(json0);
 
 const appDiv: HTMLElement = document.getElementById('app');
+
 appDiv.innerHTML = `<h1>Naive-ui menu data generator</h1>
-<p>
-Please open console
-</p>
+<p>Please open console</p>
 
 <ul>
-  <li><a href="#json1">Source data</a></li>
+  <li><a href="#json1">Menu</a></li>
+  <li><a href="#json0">API data</a></li>
 </ul>
-<div id="json1">
+
 <hr>
-<h3>Source data</h3>
+
+<div id="json1">
+<h3>Menu</h3>
 <a href="#">Top</a>
 <pre>
+${json1}
+</pre>
+</div>
 
+<div id="json0">
+<h3>API data</h3>
+<a href="#">Top</a>
+<pre>
+${json0}
 </pre>
 </div>
 
