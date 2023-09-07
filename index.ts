6ca951100c848a70bb781cb3f74e555e9a583adb
clabnet <clabnet@users.noreply.github.com>
2023-09-07 11:15:28 +0200

    4 files modified, 1 added

--- index.ts
+++ index.ts
@@ -1,6 +1,25 @@
-// Import stylesheets
 import './style.css';
+import * as _ from 'lodash';
+import bom from './data/sourceData.json';
 
-// Write TypeScript code!
+// const json0 = JSON.stringify(data, null, 2);
+
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>Naive-ui menu data generator</h1>
+<p>
+Please open console
+</p>
+
+<ul>
+  <li><a href="#json1">Source data</a></li>
+</ul>
+<div id="json1">
+<hr>
+<h3>Source data</h3>
+<a href="#">Top</a>
+<pre>
+
+</pre>
+</div>
+
+`;
